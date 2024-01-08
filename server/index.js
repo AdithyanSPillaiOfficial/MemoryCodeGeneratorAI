2526691541d606bb95d6a51f80c9e4b6d8fb493c
AdithyanSPillaiOfficial <adithyanspillaiofficial@gmail.com>
2024-01-08 23:34:21 +0530

    Server Changes

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const { OpenAI } = require("openai");
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
 });
